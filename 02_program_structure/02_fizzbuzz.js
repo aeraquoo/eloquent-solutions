f2d31b62cc24c3a0cc0ed824ca93b8bed8040711
Jaye Heffernan <jpeheffernan@gmail.com>
2016-04-13 19:27:26 +1000

    Modified fizzbuzz

--- 02_program_structure/02_fizzbuzz.js
+++ 02_program_structure/02_fizzbuzz.js
@@ -1,13 +1,22 @@
 for (var i=1; i <= 100; i++) {
-  if (i % 3 === 0) {
+  var three = (i % 3 === 0);
+  var five =  (i % 5 === 0);
+
+  if (three && five) {
+    // Divisible by five and three
+    console.log("FizzBuzz");
+  }
+  else if (three) {
     // Divisible by 3
     console.log("Fizz");
   }
-  else if (i % 5 === 0) {
+  else if (five) {
     // Divisible by 5
     console.log("Buzz");
   }
   else {
+    // Not divisible by five or three, just log the number
     console.log(i);
   }
+
 }
