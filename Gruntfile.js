c203d520da78a4b2fec02c3afd9dd55357451324
Jaye Heffernan <jpeheffernan@gmail.com>
2016-04-17 13:26:21 +1000

    Added Gruntfile for automated, continuous testing and linting

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      options: {
+        // Enforce === over ==
+        eqeqeq: true,
+        // Don't allow use of variables before definition, functions ok though
+        latedef: "nofunc",
+        // Check for unused variables and parameters
+        unused: "strict",
+        globals: {
+          mocha: true,
+          node: true
+        },
+        force: true,
+      },
+      all: ['Gruntfile.js', '0**/**.js']
+    },
+    mochaTest: {
+      test: {
+        options: {
+          reporter: "spec",
+          clearRequireCache: true
+        },
+        src: ['**/test/*.test.js']
+      }
+    },
+    watch: {
+      options: {
+        spawn: true,
+        interrupt: true
+      },
+      files:['Gruntfile.js', '*/**.js'],
+      tasks: ['jshint', 'mochaTest']
+    }
+  });
+
+  // Load the plugin that provides the "jshint" task.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-karma');
+
+  // Default task(s).
+  grunt.registerTask('default', ['jshint', 'mochaTest']);
+
+};
