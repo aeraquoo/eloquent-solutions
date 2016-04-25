228cf4caf88f4a0c5bc9d78068af5cab6a29f7b2
Jaye Heffernan <jpeheffernan@gmail.com>
2016-04-25 19:59:42 +1000

    planned breaking up electronic life into modules

---
+++ 10_modules/02_a_return_to_electronic_life.js
@@ -0,0 +1,29 @@
+mar answer = `
+
+# Low Level World Stuff
+
+Vector
+Grid
+directions
+directionNames
+randomElement
+elementFromChar
+
+# High Level World Stuff
+
+World
+charFromElement
+Wall
+View
+dirPlus
+LifelikeWorld
+Plant
+
+# Critters
+
+BouncingCritter
+WallFollower
+PlantEater
+SmartPlantEater
+Tiger
+`;
