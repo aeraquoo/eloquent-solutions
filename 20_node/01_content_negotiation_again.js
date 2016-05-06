220fee5ea0012a5e9408ce4dbd1b5cd2af8f2007
Jaye Heffernan <jaye@administrators-Mac-mini.local>
2016-05-06 12:26:54 +1000

    experiment with Accept headers again, this time in Node

---
+++ 20_node/01_content_negotiation_again.js
@@ -0,0 +1,37 @@
+var http = require("http");
+
+ctypes = ["text/plain", "text/html", "application/json"];
+
+// For each content type...
+ctypes.forEach( function(type) {
+    // make an HTTP request to author end-point
+    http.request({
+        hostname: "eloquentjavascript.net",
+        path: "/author",
+        method: "GET",
+        headers: {
+            // Accept the targeted content-type
+            Accept: type
+        }
+    }, HttpHandler(type)).end();
+});
+
+/**
+ * Factory function for HTTP response handler functions
+ *
+ * @param {string} type - the content-type used in the Accept header of the request (for
+ * logging purposes)
+ * @returns {function} handleResponse - a response-handler which know about `type`
+ */
+function HttpHandler(type) {
+    return function(response) {
+        response.setEncoding("utf-8");
+        var body = "";
+        response.on("data", function(chunk) {
+            body += chunk;
+        });
+        response.on("end", function() {
+            console.log( "\n---\n" + type + "\n---\n" + body + "\n---" );
+        });
+    };
+}
